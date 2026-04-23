21d20583ba31542f69351a5292bd2c4c0e81f535
Rishi Guntur <rguntur@uchicago.edu>
2026-04-22 19:55:50 -0500

    Fix color code for Blue Owl Capital in app.py

--- app.py
+++ app.py
@@ -335,7 +335,6 @@ if show_credit:
     for ticker, label, color in [("ARCC",   "Ares Capital (ARCC)", "#45aaf2"),
                                   ("FS_KKR", "FS KKR (FSK)",        "#fd9644"),
                                 ("BSXL", "Blackstone (BSXL)",        "#20bf6b"),
-                                ("OBDC", "Blue Owl Capital (OBDC)",        "#a55eea"),
                                 ("OBDC", "Blue Owl Capital (OBDC)",        "#3867d6"),
                                 ("TSLX", "Sixth Street (TSLX)",        "#eb3b5a")]:
         if ticker in yfd:
